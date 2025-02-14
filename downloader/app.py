3d982777711dd0cbb48731753c54ddf92bc62284
Douglas Utz <douglas@utz.net.br>
2025-02-14 00:32:07 -0300

    Ajust to delete the files after the download

--- downloader/app.py
+++ downloader/app.py
@@ -1,30 +1,53 @@
+import os
+import time
+import threading
 from flask import Flask, render_template, request, send_file
 import yt_dlp
-import os
 
 app = Flask(__name__)
 
-# Download the video using yt_dlp
+# Define the download folder
+def get_download_folder():
+    base_folder = os.path.dirname(os.path.abspath(__file__))
+    download_folder = os.path.join(base_folder, 'downloads')
+    if not os.path.exists(download_folder):
+        os.makedirs(download_folder)
+    return download_folder
+
+# Clean the files periodically
+def clean_downloads_folder():
+    download_folder = get_download_folder()
+    while True:
+        time.sleep(90)  # 10 minutes
+        
+        try:
+            for filename in os.listdir(download_folder):
+                file_path = os.path.join(download_folder, filename)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                    print(f'Arquivo removido: {file_path}')
+        except Exception as e:
+            print(f'Erro ao limpar a pasta de downloads: {e}')
+
+# Download the video
 def download_video(url):
-    
-    # Defenition of the download folder
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    download_dir = os.path.join(base_dir, 'downloads')
-    if not os.path.exists(download_dir):
-        os.makedirs(download_dir)
-    
-    # Defenition of yt_dlp options
+    download_folder = get_download_folder()
     ydl_opts = {
-        'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
+        'outtmpl': os.path.join(download_folder, '%(title)s.%(ext)s'),
         'format': 'best',
         'quiet': True
     }
 
-    # Video download
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info_dict)
         return filename
+
+# Begin the clean thread
+def start_cleanup_thread():
+    cleanup_thread = threading.Thread(target=clean_downloads_folder)
+    cleanup_thread.daemon = True
+    cleanup_thread.start()
 
 # Home route
 @app.route('/')
@@ -34,22 +57,18 @@ def index():
 # Download route
 @app.route('/download', methods=['POST'])
 def download():
-    
-    # Get the URL
     url = request.form['url']
     
     if not url:
         return 'Por favor, forneça uma URL válida.'
     
     try:
-        # Donwloads the video
         video_file = download_video(url)
-
-        # Send the vidoe to the user
         return send_file(video_file, as_attachment=True)
-
     except Exception as e:
         return f'Erro ao baixar o vídeo: {str(e)}'
 
+# Main
 if __name__ == '__main__':
-    app.run(debug=True)
+    start_cleanup_thread()
+    # app.run(debug=True)
